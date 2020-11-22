5a5a6290dd9e90391aee29728d12e0109fe0cc54
dcpasha <66342395+dcpasha@users.noreply.github.com>
2020-11-22 11:49:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,63 @@
 import pandas_datareader.data as web  # To import financial data directly into python
 import pandas as pd
 import statsmodels.api as sm
-import numpy as np
+
 
 # Let's calculate Fama-French 3 Factor Model for a stock
-# Let's do it for Microsoft Corp ($MSFT)
+def getStockPrice(stock_ticker, start_date, end_date):
+    # Get Price data for any stock for any specified duration.
+    stock = web.DataReader(stock_ticker, 'yahoo', start_date, end_date)
 
-start = '2019-09-30'
-end = '2020-09-30'
+    # Getting the daily return of the stock.
+    # We use the 'Adj Close' price of any stock.
+    # It is the closing price after adjustments for all applicable splits and dividend distributions.
+    stock = stock['Adj Close'].pct_change()[1:]
 
-# Import data for a security from yahoo finance
-stock = web.DataReader('MSFT', 'yahoo', start, end)
+    # Converting the Series to Dataframe to do all of operations.
+    stock = stock.to_frame()
 
-# Import data for three Fama-French factors
-ff_factors = pd.read_csv(
-    '/Users/pavelpotapov/PycharmProjects/FamaFrench3factorModel/F-F_Research_Data_Factors_daily.CSV', skiprows=3)
+    # Renaming the column.
+    stock.rename(columns={'Adj Close': 'Return Pct'}, inplace=True)
 
-# 9:23 am
-# TODO (DONE): Both stock and ff_factors are dataframes. We need to reformat ff_factors, so the date looks just like in stock.
-# TODO (DONE): Merge them together
-# TODO (DONE): Perform the analysis
-# TODO: Write my own function for OLS
+    return stock
 
-# We are only interested in the 'Adj Close' price of the stock, but we will also keep the 'Volume'
-# 'Adj Close' is the closing price after adjustments for all applicable splits and dividend distributions.
-# 'Volume' is how many shars are traded each day.
-stock = stock.loc[:, ['Volume', 'Adj Close']]
-stock['Return Pct'] = stock['Adj Close'].pct_change()[1:]
-stock.dropna(subset=['Return Pct'], inplace=True)
 
-# Converting an object column to date format and set it as index of the dataframe.
-ff_factors['Date'] = pd.to_datetime(ff_factors['Unnamed: 0'], format='%Y%m%d', errors='coerce')
-ff_factors.set_index('Date', inplace=True)
+def getFamaFrench3Factor():
+    ff_factors = pd.read_csv(
+        '/Users/pavelpotapov/PycharmProjects/FamaFrench3factorModel/F-F_Research_Data_Factors_daily.CSV', skiprows=3)
 
-# Drop the useless column and the last row because it contains information about the data source.
-ff_factors.drop(columns='Unnamed: 0', inplace=True)
-ff_factors.drop(ff_factors.tail(1).index, inplace=True)
+    # Converting an object column to date format and set it as index of the dataframe.
+    ff_factors['Date'] = pd.to_datetime(ff_factors['Unnamed: 0'], format='%Y%m%d', errors='coerce')
+    ff_factors.set_index('Date', inplace=True)
 
-# Merging two dataframes using their indexes. It is equivalent to SQL inner join statement.
-df = pd.merge(stock, ff_factors, left_index=True, right_index=True)
-df.drop(columns=['Volume', 'Adj Close'], inplace=True)
-df['Excess returns'] = df['Return Pct'] - df['RF']
+    # Drop the unnecessary column and rename some columns.
+    ff_factors.drop(columns='Unnamed: 0', inplace=True)
+    ff_factors.rename(columns={'Mkt-RF': 'mrk_rf'}, inplace=True)
+    # Drop the last row because it contains information about the data source.
+    ff_factors.drop(ff_factors.tail(1).index, inplace=True)
 
-# Modeling.
-# Do we need to do this? Divide them by 100 because our factors are whole numbers and not percentages.
-# ff_factors/100
-df1 = ff_factors.copy()
-df1 = df1.apply(lambda x: x/100)
+    # Converting our factor data to percentages.
+    ff_factors = ff_factors.apply(lambda x: x / 100)
 
-df1.rename(columns={'Mkt-RF':'mrk_rf'}, inplace=True)
+    return ff_factors
 
-df1 = pd.merge(stock, df1, left_index=True, right_index=True)
-df1.drop(columns=['Volume', 'Adj Close'], inplace=True)
-df1['Excess_returns'] = df1['Return Pct'] - df1['RF']
 
-model = sm.formula.ols(formula="Excess_returns ~ mrk_rf + SMB + HML", data=df1)
-results = model.fit()
+def modelingFamaFrench(stock_ticker, start_date, end_date):
+    stock = getStockPrice(stock_ticker,start_date,end_date)
+    factors = getFamaFrench3Factor()
+
+    # Merging two dataframes using their indexes. It is equivalent to SQL inner join statement.
+    df = pd.merge(stock, factors, left_index=True, right_index=True)
+
+    # Calculating 'Excessive Returns'
+    df['Excess_returns'] = df['Return Pct'] - df['RF']
+
+    # Modeling
+    model = sm.formula.ols(formula="Excess_returns ~ mrk_rf + SMB + HML", data=df)
+    result = model.fit()
+
+    return result
+
+# Now we can calculate the alpha and beta of any stock returns against the Fama & French’s 3 factors model.
+results = modelingFamaFrench('MSFT', '2019-09-30', '2020-09-30')
 print(results.summary())
-
-# Now you know how to calculate the alpha and beta of any portfolio returns against the Fama & French’s 3 factors model.
